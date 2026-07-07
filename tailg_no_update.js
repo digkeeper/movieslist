4fc99a1720d58fca541b99c405da025cb0810b1a
digkeeper <38878661+digkeeper@users.noreply.github.com>
2026-07-07 14:22:49 +0800

    Update tailg_no_update.js

--- tailg_no_update.js
+++ tailg_no_update.js
@@ -3,4 +3,4 @@ hostname = %APPEND% www.tailgdd.com
 
 [rewrite_local]
 # 台铃智能 - 更新检测（修改响应，消除弹窗）
-^https?:\/\/www\.tailgdd\.com\/v1\/api\/app\/version\/getVersion(\?.*)?$ url script-response-body tailg_no_update.js
+^https?:\/\/www\.tailgdd\.com\/v1\/api\/app\/version\/getVersion(\?.*)?$ url reject
